475c89ee3f15807ac4cdda052fa188f5657d18d6
Ryan Sheridan <0xryann@protonmail.com>
2022-04-11 15:35:02 +0000

    Initial Flask App

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def index():
     return render_template('index.html')
 
 @app.route("/youtubedl/<v_id>/<v_type>")
-def rd_download(v_id, v_type):9
+def rd_download(v_id, v_type):
 	# if redirect to download page, get video id and type and call download function
 	title, filename, thumbnail_url = download_video(v_id, v_type)
 	filenames.append(filename)
