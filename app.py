a88e919002d8b6f81fac52e41c09e9dbf2900538
Ryan Sheridan <103150363+ryan-sheridan@users.noreply.github.com>
2022-04-14 19:30:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, send_file
 import os
 import time
 import hashlib
+import re
 
 app = Flask(__name__)
 
@@ -21,12 +22,25 @@ def clean_dl(filename):
 def download_video(v_id, v_type):
 	from pytube import YouTube
 
+	try:
+		yt = YouTube('http://youtube.com/watch?v={}'.format(v_id))
+		error_code = 0
+	except pytube.exceptions.RegexMatchError:
+		error_code = 1
+		return 0, 0, 0, error_code
+
 	if v_type == 'mp4':
-		yt = YouTube('http://youtube.com/watch?v={}'.format(v_id))
-		video = yt.streams.get_highest_resolution().download('dl/')
+		try:
+			video = yt.streams.get_highest_resolution().download('dl/')
+		except pytube.exceptions.VideoUnavailable:
+			error_code = 3
+			return 0, 0, 0, error_code
 	if v_type == 'mp3':
-		yt = YouTube('http://youtube.com/watch?v={}'.format(v_id))
-		video = yt.streams.filter(only_audio=True).first().download('dl/')
+		try:
+			video = yt.streams.filter(only_audio=True).first().download('dl/')
+		except pytube.exceptions.VideoUnavailable:
+			error_code = 3
+			return 0, 0, 0, error_code
 
 	timestamp = str(time.time())
 
@@ -38,32 +52,51 @@ def download_video(v_id, v_type):
 	defaultFilePath = 'dl/' + yt.streams.get_highest_resolution().default_filename
 	os.rename(defaultFilePath, destinationFilePath)
 
-	return yt.title, filename, yt.thumbnail_url
+	return yt.title, filename, yt.thumbnail_url, error_code
 
 @app.route("/")
-def index(warningMessage=0):
-	# return index if no subdir is called
-	if warningMessage == 0:
-		warningMessage = ''
-	else:
-		warningMessage = 'There was an error with the link you entered, please check formatting and try again.'
-	return render_template('index.html', warningMessage=warningMessage)
+def index(errorCode=0):
+	if errorCode == 0:
+		warningStr = ''
+		return render_template('index.html')
+	if errorCode == 1:
+		warningStr = 'there has been a problem trying to process the video url/id, please check if the input is correct and try again.'
+		return warningStr + str(errorCode)
+	if errorCode == 2:
+		warningStr = 'please enter a video id or video url into the input box.'
+		return warningStr + str(errorCode)
+	if errorCode == 3:
+		warningStr = 'this video does not exist, please try again with a valid link.'
+		return warningStr + str(errorCode)
+	if errorCode == 4:
+		warningStr = 'please stop.'
+		return warningStr + str(errorCode)
 
-@app.route("/youtubedl//<v_type>")
+
+@app.route("/youtubedl/<v_type>")
 def throwNoLinkError(v_type):
-	return index(warningMessage=1)
+	return index(errorCode=2)
 
 @app.route("/youtubedl/<v_id>/<v_type>")
 def rd_download(v_id, v_type):
 	# if redirect to download page, get video id and type and call download function
-	title, filename, thumbnail_url = download_video(v_id, v_type)
-	filenames.append(filename)
-	print(filenames)
-	return render_template('youtubedl.html', filename=filename, title=title, v_type=v_type, thumbnail_url=thumbnail_url)
+	title, filename, thumbnail_url, error_code = download_video(v_id, v_type)
+
+	if error_code == 1:
+		return index(errorCode=error_code)
+	if error_code == 2:
+		return index(errorCode=error_code)
+	if error_code == 3:
+		return index(errorCode=error_code)
+	else:
+		return render_template('youtubedl.html', filename=filename,
+												 title=title,
+												 v_type=v_type,
+												 thumbnail_url=thumbnail_url,
+												 error_code=error_code)
 
 @app.route("/d/<filename>")
 def download_file(filename):
-	filenames.remove(filename)
 	try:
 		path = 'dl/' + filename
 		return send_file(path)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,33 @@
+function returnHome(){
+	window.location.href = "/";
+}
+
+function createWarningMessage(warningMessage){
+	var warningElement = document.getElementById("warningElement");
+	warningElement.innerHTML = warningMessage
+}
+
 function redirectToSite(){
 	const videoId = document.getElementById('videoidbox').value;
 	const videoType = document.getElementById('videotype').value;
-	window.location.href = "/youtubedl/" + videoId + "/" + videoType;
+	let xhr = new XMLHttpRequest();
+	xhr.open('get', '/youtubedl/' + videoId + '/' + videoType);
+	xhr.send();
+
+	xhr.onload = function() {
+		if (xhr.response[0] == 0){
+			document.write('')
+			document.write(xhr.response.substring(1))
+		} else {
+			createWarningMessage(xhr.response.slice(0, -1))
+		}
+		console.log(xhr.response)
+	};
+
+}
+
+async function value(){
+	const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3').then(FingerprintJS => FingerprintJS.load())
+	const a = await fpPromise.then(fp => fp.get()).then(result => {return result.visitorId})
+	console.log(a)
 }
